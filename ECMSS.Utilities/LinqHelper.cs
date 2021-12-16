5fbb6be98e9a028ca78520f37606f1e3ea8fa8c8
ct14anhhuy <ct14anhhuy@gmail.com>
2021-12-16 16:42:16 +0700

    update 16/12/2021

--- ECMSS.Utilities/LinqHelper.cs
+++ ECMSS.Utilities/LinqHelper.cs
@@ -1,5 +1,6 @@
 ﻿using ECMSS.Data;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -7,42 +8,24 @@ namespace ECMSS.Utilities
 {
     public static class LinqHelper
     {
-        private static readonly string[] powerPointTypes = new string[] { "ppt", "pptx" };
-        private static readonly string[] excelTypes = new string[] { "xls", "xlt", "xlsx", "xlsm", "xlsb", "xltx", "xltm", "csv" };
-        private static readonly string[] wordTypes = new string[] { "doc", "docx" };
-        private static readonly string[] pdfTypes = new string[] { "pdf" };
-        private static readonly string[] imageTypes = new string[] { "jpg", "gif", "png", "jpeg" };
-        private static readonly string[] cadTypes = new string[] { "dwg", "dwt", "dxf" };
-        private static readonly string[] videoTypes = new string[] { "webm", "mpeg", "ogg", "mp4", "mpg", "mpv", "m4p", "m4v", "avi", "wmv", "flv", "swf" };
-
         public static Expression<Func<FileInfo, bool>> GetFromType(string type)
         {
-            switch (type.ToUpper())
+            Dictionary<string, string[]> fileTypes = new Dictionary<string, string[]>
             {
-                case "POWERPOINT":
-                    return f => powerPointTypes.Contains(f.Extension);
-
-                case "EXCEL":
-                    return f => excelTypes.Contains(f.Extension);
-
-                case "WORD":
-                    return f => wordTypes.Contains(f.Extension);
-
-                case "PDF":
-                    return f => pdfTypes.Contains(f.Extension);
-
-                case "IMAGE":
-                    return f => imageTypes.Contains(f.Extension);
-
-                case "CAD":
-                    return f => cadTypes.Contains(f.Extension);
-
-                case "VIDEO":
-                    return f => videoTypes.Contains(f.Extension);
-
-                default:
-                    return f => true;
+                { "POWERPOINT", new string[] { "ppt", "pptx" } },
+                { "EXCEL", new string[] { "xls", "xlt", "xlsx", "xlsm", "xlsb", "xltx", "xltm", "csv" } },
+                { "WORD", new string[] { "doc", "docx" } },
+                { "PDF", new string[] { "pdf" } },
+                { "IMAGE", new string[] { "jpg", "gif", "png", "jpeg" } },
+                { "CAD", new string[] { "dwg", "dwt", "dxf" } },
+                { "VIDEO", new string[] { "webm", "mpeg", "ogg", "mp4", "mpg", "mpv", "m4p", "m4v", "avi", "wmv", "flv", "swf" } }
+            };
+            var types = fileTypes.Where(x => x.Key == type.ToUpper()).Select(x => x.Value).FirstOrDefault();
+            if (types != null)
+            {
+                return f => types.Contains(f.Extension);
             }
+            return f => true;
         }
     }
 }
